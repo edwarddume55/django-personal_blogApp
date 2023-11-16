07e825b591b419e20a8791867afd4a26e0c0900d
Edward Dume <edwarddume55@gmail.com>
2023-11-16 09:38:57 +0300

    adding views logic and creating templates

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
 #Post model
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -13,9 +19,14 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
 
+    def __str__(self):
+        return self.title
 #Comment model
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
