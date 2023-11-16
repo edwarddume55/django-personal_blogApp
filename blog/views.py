15f31a9807926cfc2b945e00504d9597b4234439
Edward Dume <edwarddume55@gmail.com>
2023-11-16 10:17:30 +0300

    adding post commenting/reply form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
-
+from blog.forms import CommentForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def blog_index(request):
@@ -25,10 +26,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
-
     return render(request, "blog/detail.html", context)
